49f75198a5154cce50b276fb3fd73d820b4b23fc
JosephatJuma <jumajosephat61@gmail.com>
2025-12-12 15:52:55 +0300

    update auth screens and context

---
+++ src/components/reusable/AuthLoadingScreen.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { ActivityIndicator, Text } from "react-native-paper";
+import { useThemeContext } from "../../contexts/ThemeContext";
+
+const AuthLoadingScreen: React.FC = () => {
+  const { theme } = useThemeContext();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: theme.colors.background },
+      ]}
+    >
+      <ActivityIndicator size="large" color={theme.colors.primary} />
+      <Text
+        variant="bodyLarge"
+        style={[styles.text, { color: theme.colors.onBackground }]}
+      >
+        Loading...
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    marginTop: 16,
+  },
+});
+
+export default AuthLoadingScreen;
